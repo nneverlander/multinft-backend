10453a5b627772f9063967a48d0c8a11ca5e5909
Adi Kancherla <findadit@gmail.com>
2019-05-26 15:42:12 -0700

    contract interaction works

--- index.js
+++ index.js
@@ -1,79 +1,94 @@
-const web3 = require("web3");
-const express = require("express");
-const Tx = require("ethereumjs-tx");
-const app = express();
-const crypto = require("crypto");
-const truffleContract = require("truffle-contract");
 require("dotenv").config();
 
+const express = require("express");
+const app = express();
 app.use(express.json());
-
-const port = process.env.PORT;
-const web3Provider = new web3.providers.HttpProvider(process.env.WEB3_PROVIDER);
-const web3js = new web3(web3Provider, undefined, {
-    transactionConfirmationBlocks: 1
-});
 
 const firebase = require("firebase-admin");
 firebase.initializeApp();
-
-const multiNFTJson = require("./MultiNFT.json");
-
 const db = firebase.firestore();
-const configRef = db.collection("config").doc("current");
-// const huntersRef = rootRef.doc(process.env.FIREBASE_HUNTERS_REF);
-// const huntersKeysRef = huntersRef.collection(process.env.FIREBASE_KEYS_REF);
+const configRef = db.collection("config");
+const firestoreRootRef = process.env.FIRESTORE_ROOT_REF || "prod";
+const rootRef = db.collection(firestoreRootRef);
 
-// async function sendSatTreasureKeyNFT(hunter) {
-//     const fromAddress = process.env.SATOSHI_TREASURER_APPROVER_ADDR;
-//     const privateKey = Buffer.from(
-//         process.env.SATOSHI_TREASURER_APPROVER_PRIV_KEY,
-//         "hex"
-//     );
-//     const contractAddress = process.env.MAVRIK_CONTRACT_ADDR;
-//     const mavrik = new web3js.eth.Contract(mavrikJson.abi, contractAddress);
-//     // get transaction count, later will used as nonce
-//     let count = await web3js.eth.getTransactionCount(fromAddress, "pending");
-//     let rawTransaction = {
-//         from: fromAddress,
-//         gasPrice: web3js.utils.toHex(20 * 1e9),
-//         gasLimit: web3js.utils.toHex(210000),
-//         to: contractAddress,
-//         data: mavrik.methods
-//             .mintNonFungible(process.env.SATOSHI_TREASURE_KEY_NFT_TYPE, [
-//                 hunter
-//             ])
-//             .encodeABI(),
-//         nonce: web3js.utils.toHex(count)
-//     };
-//     //console.log(rawTransaction);
-//     let transaction = new Tx(rawTransaction);
-//     transaction.sign(privateKey);
-//     let txResult = await web3js.eth.sendSignedTransaction(
-//         "0x" + transaction.serialize().toString("hex")
-//     );
-//     console.log(txResult);
-//     return txResult;
-// }
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const truffleContract = require("truffle-contract");
+const multiNFTJson = require("./MultiNFT.json");
+const MultiNFT = truffleContract(multiNFTJson);
+let multiNFTInstance;
+
+const port = process.env.PORT || 3000;
+
+init();
+
+async function init() {
+    let provider = process.env.WEB3_PROVIDER;
+    let contractAddress = process.env.MULTINFT_CONTRACT_ADDR;
+    let privKey = process.env.PRIV_KEY;
+    let fromAddress = process.env.FROM_ADDRESS;
+    let gasLimit = process.env.GAS_LIMIT;
+
+    let currentConfig = await configRef.doc("current").get();
+    let version = currentConfig.data().version;
+    let config = await configRef.doc(version).get();
+    let configData =config.data();
+
+    if (!provider) {
+        provider = configData.WEB3_PROVIDER;
+    }
+    if (!contractAddress) {
+        contractAddress = configData.MULTINFT_CONTRACT_ADDR;
+    }
+    if (!privKey) {
+        privKey = configData.PRIV_KEY;
+    }
+    if (!fromAddress) {
+        fromAddress = configData.FROM_ADDRESS;
+    }
+    if (!gasLimit) {
+        gasLimit = configData.GAS_LIMIT;
+    }
+
+    let web3Provider = new HDWalletProvider(privKey, provider);
+
+    MultiNFT.setProvider(web3Provider);
+    MultiNFT.defaults({
+        from: fromAddress,
+        gas: gasLimit,
+        value: 0
+    })
+
+    multiNFTInstance = await MultiNFT.at(contractAddress);
+}
 
 async function addToFirebase() {
     let data = {
         createdAt: firebase.firestore.FieldValue.serverTimestamp(),
         updatedAt: firebase.firestore.FieldValue.serverTimestamp()
     };
-    let currentEnv = await configRef.get();
-    let rootRef = currentEnv.data().env;
-    let result = await db.collection(rootRef).add(data);
+    let result = await rootRef.add(data);
     return result;
 }
 
-// app.get("/", async function(req, res) {
-//     let result = await addToFirebase();
-//     res.send(result);   
-// });
+app.get("/", async function(req, res) {
+    // let result = await addToFirebase();
+    // res.send(result);
+    try {
+        let result = await multiNFTInstance.createType("ma2s1avav", "mv2saa1v", "vv");
+        res.send(result);
+    } catch (err) {
+        console.log(err);
+    }
+
+});
 
 app.post("/create", async function(req, res) {
-    
+    try {
+        let result = await multiNFTInstance.createType("ma2s1avv", "mv2sa1v", "vv");
+        console.log(result);
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 app.post("/mint", async function(req, res) {
